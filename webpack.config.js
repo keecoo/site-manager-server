f765d07c6a193836c0ca2ad0e46329444a51b437
Keenan Coombs <keenan.coombs@healthcatalyst.com>
2019-03-31 18:46:37 -0600

    set webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
   externals: [nodeExternals()],
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
   module: {
     rules: [
       {
